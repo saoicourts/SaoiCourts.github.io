bc05bbb6d52e92e3ac3a5ddb5dedd98242f987ad
NicoCourts <NicoCourts@users.noreply.github.com>
2023-01-09 04:41:45 +0000

    deploy: 5231909b0986cde619a4570af18d51055078e420

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "On Diversity",
+        "excerpt":"Recently I have been having a lot of thoughts about diversity in mathematics (and STEM at large) and what it means to be a white cis male in favor of diversity. Below are some of my thoughts on the matter. If you have any ideas or input feel free to...","categories": [],
+        "tags": ["diversity","math"],
+        "url": "/on-diversity/",
+        "teaser": "/assets/images/vector.webp"
+      },{
+        "title": "Racism and Police Brutality",
+        "excerpt":"Warning: This post explicitly discusses the murder of human beings by other human beings. As always when broaching such a topic, I must preface this discussion with a disclaimer. I am a white cis male American academic and all of these descriptors endow me with privilege that is simply not...","categories": [],
+        "tags": ["diversity","blm","capitalism","politics"],
+        "url": "/racism/",
+        "teaser": "/assets/images/vector.webp"
+      },{
+        "title": "Power and Class",
+        "excerpt":"The world really seems to have fallen to pieces. We’re deep into the throes of a global pandemic where hundreds of thousands of Americans have died and millions of people across the world have experienced discomfort, loss of work or wages or housing, and/or loss of loved ones. It also...","categories": [],
+        "tags": ["socialism","class","power","politics"],
+        "url": "/power/",
+        "teaser": "/assets/images/vector.webp"
+      }]
