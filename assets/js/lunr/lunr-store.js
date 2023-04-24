50c1590c4e49fc447b17b8b787fb2855442f4230
NicoCourts <NicoCourts@users.noreply.github.com>
2023-04-24 22:37:12 +0000

    deploy: 3c981bfda9c7580e008dff31989ca6a02f679337

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,53 +3,65 @@ var store = [{
         "excerpt":"Recently I have been having a lot of thoughts about diversity in mathematics (and STEM at large) and what it means to be a white cis male in favor of diversity. Below are some of my thoughts on the matter. If you have any ideas or input feel free to...","categories": [],
         "tags": ["diversity","math"],
         "url": "/on-diversity/",
-        "teaser": "/assets/images/vector.webp"
+        "teaser": "/assets/images/cat_teaser.png"
       },{
         "title": "Racism and Police Brutality",
         "excerpt":"Warning: This post explicitly discusses the murder of human beings by other human beings. As always when broaching such a topic, I must preface this discussion with a disclaimer. I am a white cis male American academic and all of these descriptors endow me with privilege that is simply not...","categories": [],
         "tags": ["diversity","blm","capitalism","politics"],
         "url": "/racism/",
-        "teaser": "/assets/images/vector.webp"
+        "teaser": "/assets/images/cat_teaser.png"
       },{
         "title": "Power and Class",
         "excerpt":"The world really seems to have fallen to pieces. We’re deep into the throes of a global pandemic where hundreds of thousands of Americans have died and millions of people across the world have experienced discomfort, loss of work or wages or housing, and/or loss of loved ones. It also...","categories": [],
         "tags": ["socialism","class","power","politics"],
         "url": "/power/",
-        "teaser": "/assets/images/vector.webp"
+        "teaser": "/assets/images/cat_teaser.png"
+      },{
+        "title": "Support in Representation Theory and Beyond",
+        "excerpt":"For quite a while I have been planning on writing a blog post on something that I know about more than the average Joe. My crippling case of impostor syndrome in math, however, has kept me from using this space to write anything related to my work in math (you’d...","categories": [],
+        "tags": ["math","rep-theory","alg-geom"],
+        "url": "/support-theory/",
+        "teaser": "/assets/images/cat_teaser.png"
+      },{
+        "title": "Stable Democracy",
+        "excerpt":"I hate politics. Ask my friends or my partner or my parents and you might come to believe that I feel differently. But that’s just because I also happen to think that politics is one of the most important things in our lives. We are social creatures and everything great...","categories": [],
+        "tags": ["democracy","politics","essays"],
+        "url": "/society/",
+        "teaser": "/assets/images/cat_teaser.png"
       },{
         "title": "Bundle Networks: Fiber Bundles, Local Trivializations, and a Generative Approach to Exploring Many-to-one Maps",
         "excerpt":"Abstract: Many-to-one maps are ubiquitous in machine learning, from the image recognition model that assigns a multitude of distinct images to the concept of “cat” to the time series forecasting model which assigns a range of distinct time-series to a single scalar regression value. While the primary use of such...","categories": [],
         "tags": [],
         "url": "/publications/bundlenets/",
-        "teaser": "/assets/images/vector.webp"
+        "teaser": "/assets/images/cat_teaser.png"
       },{
         "title": "Representations and Support Theory for Bosonized Quantum Complete Intersections",
         "excerpt":"Abstract: Support theories are frequently used by representation theorists when trying to understand module categories with complicated structure. We associate to an algebra A a variety where the topological structure is determined by the support of modules over A. Support comes in many flavors, from the easily described (but computationally...","categories": [],
         "tags": [],
         "url": "/publications/dissertation/",
-        "teaser": "/assets/images/vector.webp"
+        "teaser": "/assets/images/cat_teaser.png"
       },{
         "title": "Fiber Bundle Morphisms as a Framework for Modeling Many-to-Many Maps ",
         "excerpt":"Abstract: While it is not generally reflected in the `nice’ datasets used for benchmarking machine learning algorithms, the real-world is full of processes that would be best described as many-to-many. That is, a single input can potentially yield many different outputs (whether due to noise, imperfect measurement, or intrinsic stochasticity...","categories": [],
         "tags": [],
         "url": "/publications/fb-morphisms/",
-        "teaser": "/assets/images/vector.webp"
+        "teaser": "/assets/images/cat_teaser.png"
       },{
         "title": "Fuzzy Simplicial Networks: A Topology-Inspired Model to Improve Task Generalization in Few-shot Learning",
         "excerpt":"Abstract: Deep learning has shown great success in settings with massive amounts of data but has struggled when data is limited. Few-shot learning algorithms, which seek to address this limitation, are designed to generalize well to new tasks with limited data. Typically, models are evaluated on unseen classes and datasets...","categories": [],
         "tags": [],
         "url": "/publications/fuzzy/",
-        "teaser": "/assets/images/vector.webp"
+        "teaser": "/assets/images/cat_teaser.png"
       },{
         "title": "Schur Duality and Strict Polynomial Functors",
         "excerpt":"Abstract: We begin by going through a considerable amount of domain knowledge concerning representations of GL⁡n\\operatorname{GL}_nGLn​, representations of Sn\\mathfrak S_nSn​, tracing the development from the classical study of group representations by Schur and Weyl and the transformation of this theory in the more robust language of affine group schemes. From...","categories": [],
         "tags": [],
         "url": "/publications/masters-thesis/",
-        "teaser": "/assets/images/vector.webp"
+        "teaser": "/assets/images/cat_teaser.png"
       },{
         "title": "One Representation to Rule Them All: Identifying Out-of-Support Examples in Few-shot Learning with Generic Representations",
         "excerpt":"Abstract: The field of few-shot learning has made remarkable strides in developing powerful models that can operate in the small data regime. Nearly all of these methods assume every unlabeled instance encountered will belong to a handful of known classes for which one has examples. This can be problematic for...","categories": [],
         "tags": [],
         "url": "/publications/out-of-support/",
-        "teaser": "/assets/images/vector.webp"
+        "teaser": "/assets/images/cat_teaser.png"
       }]
